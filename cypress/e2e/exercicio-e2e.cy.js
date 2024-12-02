e6ea484a0f4df77f8215b103dfc7094f45661e05
Diego <droxo63@gmail.com>
2024-12-02 12:21:41 -0300

    first commit

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -10,10 +10,26 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       E validando minha compra ao final */
 
   beforeEach(() => {
-      cy.visit('/')
+      cy.visit('minha-conta')
   });
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+    cy.fixture('perfil').then((dados) => {
+        cy.login(dados.usuario, dados.senha)
+        cy.get('#tbay-main-content').should('contain', 'Olá, aluno_ebac (não é aluno_ebac? Sair)')
+// rotina de escolher o primeiro produto
+
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+        cy.get('.product-block').first().click()
+        cy.get('.button-variable-item-XS').click()
+        cy.get('.button-variable-item-Yellow').click()
+        cy.get('.button-variable-item-Yellow').click()
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.woocommerce-message').should('contain', '“Aero Daily Fitness Tee” foi adicionado no seu carrinho.')
+        cy.get('.woocommerce-message > .button').click()
+        //cy.get('.product-name > a'). should('contain','Aero Daily Fitness Tee - XS, Yellow')
+    })
+    //cy.get('.page-title').should('contain', 'Minha conta')
       //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
       
   });
